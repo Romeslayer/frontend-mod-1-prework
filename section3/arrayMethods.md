5ba19024bff460efd326247e98decc8dd1a65f48
DeNaje Ferguson <denaje.ferguson@gmail.com>
2021-10-17 16:47:25 -0600

    Add section 3

---
+++ section3/arrayMethods.md
@@ -0,0 +1,9 @@
+# Array Methods
+### Pop
+* The `exampleArray.pop()` Array method is used to remove the last item from the array. The returned value of `.pop()` method is the item it removed from the end of the array, if the array is empty then the returned value is `undefined`.
+### Push
+* The `exampleArray.push()` Array method is used to add an item to the end of the array. You can add multiple items to the end of the array by feeding multiple arguments into the `.push()` method.
+### Shift
+* The `exampleArray.shift()` Array method is used to remove the beginning item of the array. The returned value of the `.shift()` method is the item it removed, if the array is empty then the returned value is `undefined`.
+### Unshift
+* The `exampleArray.unshift()` Array method is used to add an item to the beginning of the array. You could add multiple items to the beginning of the array by feeding multiple arguments into the `.unshift()` method.
--- section3/exercises/loops.js
+++ section3/exercises/loops.js
@@ -23,25 +23,43 @@ for (var i = 0; i < 3; i++) {
 }
 
 // What is logged to the console for this second example? Why?
-// EXPLAIN:
+
+// EXPLAIN: 0 then 1 then 2 is logged to the console. The loop counter i is set
+// to 0 then the condition is to check that i is less than 3, if not add one to i and run again.
+// So the program would set i to 0 then check if i is less than 3, which it is,
+// then it runs the code block and logs 0 then it adds one to i and starts the loop over again.
+// checks that i is less than 3, 1 < 3, logs 1 to the console, adds 1 to i and loop again.
+// Check that i is less than 3, 2 < 3, logs 2 to the console, adds 1 to i and loop again.
+// Check that i is less than 3, 3 is not less than 3, so the program quits the loop and does
+// not log 3 to the console.
 
 
  // YOU DO: Write code below that logs to the console, the sum of 2 plus 2, 7 times:
-
+for (var i = 0; i < 7; i++) {
+  console.log(2+2);
+}
 
  // YOU DO: Write code below that logs the String 'She sells seashells down by the seashore'
  // 10 times.
-
+for (var i = 0; i < 10; i++) {
+  console.log("She sell seashells down by the seashore");
+}
 
 //-------------------
 // LEVEL 2: An Added Layer of Challenge
 //-------------------
 
-// YOU DO: Write code below that logs to the console a String of "This is loop number: " 
+// YOU DO: Write code below that logs to the console a String of "This is loop number: "
 //that concatenates with `i`.
 
-// YOU DO: Using a for loop, how could you get an output that looks like this:
+for (var i = 1; i < 4; i++) {
+  console.log("This is loop number: " + i);
+}
 
+// YOU DO: Using a for loop, how could you get an output that looks like this:
+for (var i = 10; i >= 1; i--) {
+  console.log(i);
+}
 // 10
 // 9
 // 8
@@ -53,6 +71,6 @@ for (var i = 0; i < 3; i++) {
 // 2
 // 1
 
-// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend 
+// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend
 // more than 15 minutes without making progress. If you are at 15 minutes and haven't made
 // progress, that means it's time to reach out to your pre-work group on Slack!
